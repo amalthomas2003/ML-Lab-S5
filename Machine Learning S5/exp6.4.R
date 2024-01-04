d24edba848a872b3d5289308a3ecc4a998dc4ed4
Amal Thomas <94557654+amalthomas2003@users.noreply.github.com>
2024-01-04 22:37:47 +0530

    Add files via upload

---
+++ Machine Learning S5/exp6.4.R
@@ -0,0 +1,43 @@
+########################################    bagging
+
+library(ipred)
+library(caret)
+
+set.seed(123)
+
+#preprocessing : 1
+split_index <- createDataPartition(iris$Species, p = 0.8, list = FALSE)
+training_data <- iris[split_index, ]
+testing_data <- iris[-split_index, ]
+
+# Train the bagged model
+bagged_model <- bagging(Species ~ ., data = training_data, nbagg = 50)
+
+# Make predictions on the testing data
+predictions <- predict(bagged_model, newdata = testing_data)
+
+# Evaluate accuracy using confusion matrix
+confusionMatrix(predictions, testing_data$Species)
+ 
+ 
+ ################################      boosting
+library(gbm)
+library(caret)
+
+data(iris)
+set.seed(123)
+
+split_index <- createDataPartition(iris$Species, p = 0.8, list = FALSE)
+training_data <- iris[split_index, ]
+testing_data <- iris[-split_index, ]
+
+ans <- gbm(Species ~ ., data = training_data, distribution = "multinomial", n.trees = 100, interaction.depth = 3, shrinkage = 0.1)
+p <- predict(ans, newdata = testing_data, n.trees = 100, type = "response")
+
+class_predictions <- colnames(p)[apply(p, 1, which.max)]
+
+testing_data$Species <- as.factor(testing_data$Species)
+class_predictions <- as.factor(class_predictions)
+
+confusion_matrix <- confusionMatrix(class_predictions, testing_data$Species)
+confusion_matrix
---
+++ Machine Learning S5/exp7.2.R
@@ -0,0 +1,25 @@
+library(e1071)
+library(caret)
+
+data(iris)
+
+# Split the data into training and testing sets
+set.seed(123)
+split_index <- createDataPartition(iris$Species, p = 0.8, list = FALSE)
+training_data <- iris[split_index, ]
+testing_data <- iris[-split_index, ]
+
+# Train an SVM model whose boundary is defined by a radial function
+#use linear, sigmoid,polynomial if needed
+
+svm_model <- svm(Species ~ ., data = training_data, kernel = "radial")
+
+# Make predictions on the testing data
+predictions <- predict(svm_model, newdata = testing_data)
+
+# Convert predictions and actual values to factors
+predictions <- as.factor(predictions)
+actual_values <- as.factor(testing_data$Species)
+
+# Create confusion matrix
+confusionMatrix(predictions, actual_values)
